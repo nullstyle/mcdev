51f6badcd5f119300252c2df0dd73f42a79806f5
Scott Fleckenstein <nullstyle@gmail.com>
2015-07-25 15:13:11 -0700

    Add pkgwork.Worker

--- cmd/main/test-mctesterton/main.go
+++ cmd/main/test-mctesterton/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/nullstyle/testy-mctesterton/pkgwatch"
+	"github.com/nullstyle/testy-mctesterton/pkgwork"
 )
 
 var done = make(chan os.Signal, 1)
@@ -24,6 +25,16 @@ func main() {
 		Debounce: 300 * time.Millisecond,
 	}
 
+	worker := &pkgwork.Worker{
+		Fn: func(pkg string) error {
+			log.Printf("working on %s", pkg)
+			<-time.After(30 * time.Second)
+			log.Printf("finished with %s", pkg)
+			return nil
+		},
+		Cooldown: 5 * time.Second,
+	}
+
 	if err := watcher.Run(); err != nil {
 		log.Fatal(err)
 	}
@@ -34,7 +45,11 @@ func main() {
 	for {
 		select {
 		case pkg := <-watcher.Changes():
-			log.Printf("changed: %s", pkg)
+			go func() {
+				if err := worker.Run(pkg); err != nil {
+					log.Fatal(err)
+				}
+			}()
 		case _ = <-done:
 			log.Println("shutting down")
 			os.Exit(0)
---
+++ pkgwork/worker.go
@@ -0,0 +1,89 @@
+package pkgwork
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+type Worker struct {
+	Fn       func(string) error
+	Cooldown time.Duration
+
+	sync.Mutex
+
+	inited  bool
+	running map[string]time.Time
+	again   map[string]bool
+}
+
+func (w *Worker) Init() {
+	if w.inited {
+		return
+	}
+
+	w.running = map[string]time.Time{}
+	w.again = map[string]bool{}
+	w.inited = true
+}
+
+func (w *Worker) Run(pkg string) error {
+	w.Init()
+
+	if !w.shouldStart(pkg) {
+		return nil
+	}
+
+	defer w.finish(pkg)
+	log.Printf("starting work on %s", pkg)
+
+	err := w.Fn(pkg)
+	if err != nil {
+		return err
+	}
+
+	for w.shouldRunAgain(pkg) {
+		err := w.Fn(pkg)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (w *Worker) finish(pkg string) {
+	w.Lock()
+	delete(w.running, pkg)
+	delete(w.again, pkg)
+	w.Unlock()
+}
+
+func (w *Worker) shouldRunAgain(pkg string) bool {
+	w.Lock()
+	again, _ := w.again[pkg]
+	w.again[pkg] = false
+	w.Unlock()
+	return again
+}
+
+func (w *Worker) shouldStart(pkg string) bool {
+	w.Lock()
+
+	startedAt, alreadyRunning := w.running[pkg]
+	if alreadyRunning {
+
+		// If the cooldown has elapsed since the last start of the work for this
+		// pkg, requeue the pkg to run again after the current process is complete
+		if time.Since(startedAt) > w.Cooldown {
+			log.Printf("requeuing %s", pkg)
+			w.again[pkg] = true
+		}
+
+		w.Unlock()
+		return false
+	}
+
+	w.running[pkg] = time.Now()
+	w.Unlock()
+	return true
+}
