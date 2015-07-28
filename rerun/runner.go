9307d0ae2a38b59b80dffd3eb4fff26815d458f0
Scott Fleckenstein <nullstyle@gmail.com>
2015-07-27 21:52:31 -0700

    Remove shutdown channel, make Shutdown wait for process exit

--- rerun/runner.go
+++ rerun/runner.go
@@ -2,6 +2,7 @@ package rerun
 
 import (
 	"log"
+	"os"
 	"os/exec"
 	"time"
 )
@@ -18,12 +19,12 @@ type Runner struct {
 	cmd      *exec.Cmd
 	cooldown time.Duration
 
-	exit     chan error
-	restart  chan bool
-	shutdown chan struct{}
-	dontWait bool
-	finished bool
-	current  *exec.Cmd
+	exit      chan error
+	restart   chan bool
+	dontWait  bool
+	finishing bool
+	finished  bool
+	current   *exec.Cmd
 }
 
 // NewRunner constructs a new rerun service
@@ -32,8 +33,7 @@ func NewRunner(cmd *exec.Cmd, cooldown time.Duration) *Runner {
 		cmd:      cmd,
 		cooldown: cooldown,
 		exit:     make(chan error, 1),
-		restart:  make(chan bool, 1),
-		shutdown: make(chan struct{}),
+		restart:  make(chan bool),
 		current:  nil,
 	}
 }
@@ -53,9 +53,21 @@ func (r *Runner) Restart() {
 	r.restart <- true
 }
 
-// Shutdown causes the undelying process to stop and restart immediately
+// Shutdown kills this service runner and waits until it is complete
 func (r *Runner) Shutdown() {
-	r.shutdown <- struct{}{}
+	close(r.restart)
+
+	done := make(chan bool, 1)
+	go func() {
+		for {
+			if r.finished {
+				done <- true
+				return
+			}
+			<-time.After(1 * time.Second)
+		}
+	}()
+	<-done
 }
 
 func (r *Runner) run() {
@@ -64,25 +76,34 @@ func (r *Runner) run() {
 	for {
 		select {
 		case err := <-r.exit:
-			shouldRestart := r.finishProcess(err)
+			r.finishProcess(err)
 			r.current = nil
-
-			if shouldRestart {
-				r.start()
-			} else {
+			r.start()
+		case _, more := <-r.restart:
+			// if the restart channel closed, shutdown
+			if !more {
+				r.shuttingDown()
 				return
 			}
-		case <-r.restart:
-			r.stop()
-		case <-r.shutdown:
-			log.Println("shutting down")
-			r.finished = true
+
 			r.stop()
 		}
 	}
 }
 
-func (r *Runner) finishProcess(err error) bool {
+func (r *Runner) shuttingDown() {
+	r.finishing = true
+	select {
+	case err := <-r.exit:
+		log.Println(err)
+		r.finished = true
+		log.Println("shutdown complete")
+	case <-time.After(10 * time.Second):
+		log.Fatalln("shutdown did not complete before 10 seconds elapsed")
+	}
+}
+
+func (r *Runner) finishProcess(err error) {
 	var msg interface{}
 	var isFatal bool
 
@@ -102,16 +123,18 @@ func (r *Runner) finishProcess(err error) bool {
 	}
 
 	log.Println(msg)
-
-	return !r.finished
 }
 
 func (r *Runner) stop() {
 	log.Println("stopping service")
-	r.current.Process.Kill()
+	r.current.Process.Signal(os.Interrupt)
 }
 
 func (r *Runner) start() {
+	if r.finishing {
+		return
+	}
+
 	next := *r.cmd
 	r.current = &next
 
@@ -123,5 +146,6 @@ func (r *Runner) start() {
 	go func() {
 		log.Println("starting service")
 		r.exit <- r.current.Run()
+		log.Println("service stopped")
 	}()
 }
